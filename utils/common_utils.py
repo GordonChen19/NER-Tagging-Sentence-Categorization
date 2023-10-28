a39f7427896db9001775a25df1cb703d1f7eb794
Adrian Alviento <AlvientoAdrian.AA@gmail.com>
2023-10-28 08:37:46 +0800

    Add train, val step, earlystopper and set seed functions

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,5 +1,10 @@
 import torch
 from torch.utils.data import Dataset, DataLoader, TensorDataset
+from tqdm import tqdm 
+import time
+import os
+import random
+import numpy as np
 
 
 # function to get all words not in the word2vec model
@@ -68,6 +73,135 @@ def pad_collate(batch):
         yy_pad[i, :y_lens[i]] = y
     
     return xx_pad, yy_pad, lens
+
+
+# early stopping obtained from tutorial
+class EarlyStopper:
+    def __init__(self, patience=3, min_delta=0):
+        self.patience = patience # how many epochs to wait before stopping when loss is no longer decreasing
+        self.min_delta = min_delta # minimum difference between new loss and old loss to be considered as a decrease in loss
+        self.counter = 0 # number of epochs since loss was last decreased
+        self.min_validation_loss = np.inf # minimum validation loss achieved so far
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss: # new loss is lower than old loss
+            self.min_validation_loss = validation_loss # update minimum loss
+            self.counter = 0 # reset counter
+        elif validation_loss > (self.min_validation_loss + self.min_delta): # new loss is higher than old loss + minimum difference
+            self.counter += 1 # increase counter
+            if self.counter >= self.patience:
+                return True # stop training
+        return False # continue training
+
+
+# Train step
+def train_step(model, trainloader, optimizer, device, lossfn):
+    model.train()  # set model to training mode
+    total_loss = 0.0
+
+    # Iterate over the training data
+    for i, data in trainloader:
+        inputs, labels = data  # get the inputs and labels
+        inputs, labels = inputs.to(device), labels.to(device)  # move them to the device
+
+        optimizer.zero_grad()  # zero the gradients
+
+        # Forward pass
+        outputs = model(inputs)
+        loss = lossfn(outputs, labels)
+
+        # Backward pass and optimisation
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()  # accumulate the loss
+        trainloader.set_postfix({'Training loss': '{:.4f}'.format(total_loss/(i+1))})  # Update the progress bar with the training loss
+
+    train_loss = total_loss / len(trainloader)
+    return train_loss
+
+
+# set random seed
+def set_seed(seed = 0):
+    '''
+    set random seed
+    '''
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True 
+
+
+# Test step
+def val_step(model, valloader, lossfn, device):
+    model.eval() # set model to evaluation mode
+    total_loss = 0.0
+    correct = 0
+
+    with torch.no_grad(): # disable gradient calculation
+        for data in valloader:
+            inputs, labels = data # get the inputs and labels
+            inputs, labels = inputs.to(device), labels.to(device) # move them to the device
+
+            # Forward pass
+            outputs = model(inputs)
+            loss = lossfn(outputs, labels)
+
+            total_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1) # get the index of the max log-probability
+
+            correct += (predicted == labels).sum().item() # accumulate correct predictions
+
+    val_loss = total_loss / len(valloader)
+    accuracy = 100 * correct / len(valloader.dataset)
+
+    return val_loss, accuracy
+
+
+# Save model
+def save_model(model, path):
+    torch.save(model.state_dict(), path)
+
+
+# Training loop
+def train(model, tl, vl, opt, loss, device, epochs, early_stopper, path):
+    train_loss_list = []
+    val_loss_list = []
+    val_acc_list = []
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        start_time = time.time()  # Record the start time of the epoch
+
+        # Wrap the trainloader with tqdm for the progress bar
+        pbar = tqdm(enumerate(tl), total=len(tl), desc=f"Epoch {epoch+1}/{epochs}")
+
+        train_loss = train_step(model, pbar, opt, device, loss)  # Pass the tqdm-wrapped loader
+        val_loss, val_acc = val_step(model, vl, loss, device)
+
+        train_loss_list.append(train_loss)
+        val_loss_list.append(val_loss)
+        val_acc_list.append(val_acc)
+
+        # Print time taken for epoch
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        print(f'Epoch {epoch+1}/{epochs} took {elapsed_time:.2f}s | Train loss: {train_loss:.4f} | Val loss: {val_loss:.4f} | Val accuracy: {val_acc:.2f}% | EarlyStopper count: {early_stopper.counter}')
+
+        # save as last_model after every epoch
+        save_model(model, os.path.join(path, 'last_model.pt'))
+
+        # save as best_model if validation loss is lower than previous best validation loss
+        if val_loss < early_stopper.min_validation_loss:
+            save_model(model, os.path.join(path, 'best_model.pt'))
+
+        if early_stopper.early_stop(val_loss):
+            print('Early stopping')
+            break
+
+    return train_loss_list, val_loss_list, val_acc_list
+
 
 
 
